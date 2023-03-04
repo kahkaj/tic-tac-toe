b4f6617b5fdc4393da6e8e839a91f60683ad06ca
kahkaj <chhih.mohcine@gmail.com>
2023-03-05 00:12:36 +0100

    Add levels & change the first player per round

--- script.js
+++ script.js
@@ -3,11 +3,12 @@
 const container = document.querySelector('.container');
 const begin = document.querySelector('.begin');
 const start = document.getElementById('start');
-const warning = document.getElementById('warning');
+//const warning = document.getElementById('warning');
 const zone = document.querySelectorAll('.zone');
-const msg = document.getElementById('msg');
 const player = document.getElementById('player');
-const inputs = document.querySelectorAll('input');
+const inputs = document.querySelector('input[type="text"]');
+const blockLevels = document.querySelector('.level');
+const levels = document.querySelectorAll('input[type="radio"]');
 const end = document.querySelector('.end');
 const replay = document.getElementById('replay');
 const restart = document.getElementById('restart'); 
@@ -20,6 +21,11 @@ let moves = [],
     players = [],
     points = [0, 0];
 let lines = [];
+let [firstPlayer, secondPlayer] = ['player-1', 'player-2'];
+const color_1 = 'coral';
+const color_2 = 'cornflowerblue';
+levels[1].checked = true;
+let mode = 'medium';
 
 function getVirginZones() {
     // at first, all of zones are not clicked
@@ -56,7 +62,7 @@ function horizontalLine(arr) {
    }
 }
 
-function varticalLine(arr) {
+function verticalLine(arr) {
    for (let col = 0; col < 3; col++) {
        arr.push(board[0][col] + board[1][col] + board[2][col])
    }
@@ -67,8 +73,46 @@ function updateLinesSituation() {
     rightDiagonalLine(lines);
     leftDiagonalLine(lines);
     horizontalLine(lines);
-    varticalLine(lines);
+    verticalLine(lines);
 }
+
+function easyMode(arr) {
+    let randomZoneIndex = Math.floor(Math.random() * arr.length);
+    return zone[arr[randomZoneIndex]];
+}
+// function lineToClick(player_1) {
+//     updateLinesSituation();
+//     for (let i = 0; i < lines.length; i++) {
+//         if (lines[i] == 'OO') {
+//             return i
+//         }
+//     }
+//     for (let i = 0; i < lines.length; i++) {
+//         if (lines[i] == 'XX') {
+//             return i
+//         }
+//     }   
+//     for (let i = 0; i < lines.length; i++) {
+//         if (lines[i] == 'O') {
+//             return i
+//         }
+//     }
+//     for (let i = 0; i < lines.length; i++) {
+//         if (lines[i] == '') {
+//             return i
+//         }
+//     }
+//     for (let i = 0; i < lines.length; i++) {
+//         if (lines[i] == 'X') {
+//             return i
+//         }
+//     }
+//     for (let i = 0; i < lines.length; i++) {
+//         if (lines[i] == 'OX' || lines[i] == 'XO') {
+//             return i
+//         }
+//     }
+// }
 
 function lineToClick() {
     updateLinesSituation();
@@ -81,7 +125,7 @@ function lineToClick() {
         if (lines[i] == 'XX') {
             return i
         }
-    }
+    }   
     for (let i = 0; i < lines.length; i++) {
         if (lines[i] == 'O') {
             return i
@@ -104,160 +148,166 @@ function lineToClick() {
     }
 }
 
-function zoneToClick(line) {
-    if (line == 0) {
-        if (!zone[4].classList.contains('clicked')) {
-            return zone[4];
-        } else if (!zone[6].classList.contains('clicked')) {
-            return zone[6];
-        } else if (!zone[2].classList.contains('clicked')) {
-            return zone[2];
+function zoneToClick(line, level) {
+    if (level === 'easy') {
+        return easyMode(getVirginZones());
+    } else {
+        if (line == 0) {
+            if (!zone[4].classList.contains('clicked')) {
+                return zone[4];
+            } else if (!zone[6].classList.contains('clicked')) {
+                return zone[6];
+            } else if (!zone[2].classList.contains('clicked')) {
+                return zone[2];
+            }
         }
-    }
-    if (line == 1) {
-        if (!zone[4].classList.contains('clicked')) {
-            return zone[4];
-        } else if (!zone[8].classList.contains('clicked')) {
-            return zone[8];
-        } else if (!zone[0].classList.contains('clicked')) {
-            return zone[0];
+        if (line == 1) {
+            if (!zone[4].classList.contains('clicked')) {
+                return zone[4];
+            } else if (!zone[8].classList.contains('clicked')) {
+                return zone[8];
+            } else if (!zone[0].classList.contains('clicked')) {
+                return zone[0];
+            }
         }
-    }
-    if (line == 2) {
-        if (!zone[2].classList.contains('clicked')) {
-            return zone[2];
-        } else if (!zone[0].classList.contains('clicked')) {
-            return zone[0];
-        } else if (!zone[1].classList.contains('clicked')) {
-            return zone[1];
+        if (line == 2) {
+            if (!zone[2].classList.contains('clicked')) {
+                return zone[2];
+            } else if (!zone[0].classList.contains('clicked')) {
+                return zone[0];
+            } else if (!zone[1].classList.contains('clicked')) {
+                return zone[1];
+            }
         }
-    }
-    if (line == 3) {
-        if (!zone[4].classList.contains('clicked')) {
-            return zone[4];
-        } else if (!zone[5].classList.contains('clicked')) {
-            return zone[5];
-        } else if (!zone[3].classList.contains('clicked')) {
-            return zone[3];
+        if (line == 3) {
+            if (!zone[4].classList.contains('clicked')) {
+                return zone[4];
+            } else if (!zone[5].classList.contains('clicked')) {
+                return zone[5];
+            } else if (!zone[3].classList.contains('clicked')) {
+                return zone[3];
+            }
         }
-    }
-    if (line == 4) {
-        if (!zone[8].classList.contains('clicked')) {
-            return zone[8];
-        } else if (!zone[6].classList.contains('clicked')) {
-            return zone[6];
-        } else if (!zone[7].classList.contains('clicked')) {
-            return zone[7];
+        if (line == 4) {
+            if (!zone[8].classList.contains('clicked')) {
+                return zone[8];
+            } else if (!zone[6].classList.contains('clicked')) {
+                return zone[6];
+            } else if (!zone[7].classList.contains('clicked')) {
+                return zone[7];
+            }
         }
-    }
-    if (line == 5) {
-        if (!zone[6].classList.contains('clicked')) {
-            return zone[6];
-        } else if (!zone[0].classList.contains('clicked')) {
-            return zone[0];
-        } else if (!zone[3].classList.contains('clicked')) {
-            return zone[3];
+        if (line == 5) {
+            if (!zone[6].classList.contains('clicked')) {
+                return zone[6];
+            } else if (!zone[0].classList.contains('clicked')) {
+                return zone[0];
+            } else if (!zone[3].classList.contains('clicked')) {
+                return zone[3];
+            }
         }
-    }
-    if (line == 6) {
-        if (!zone[4].classList.contains('clicked')) {
-            return zone[4];
-        } else if (!zone[7].classList.contains('clicked')) {
-            return zone[7];
-        } else if (!zone[1].classList.contains('clicked')) {
-            return zone[1];
+        if (line == 6) {
+            if (!zone[4].classList.contains('clicked')) {
+                return zone[4];
+            } else if (!zone[7].classList.contains('clicked')) {
+                return zone[7];
+            } else if (!zone[1].classList.contains('clicked')) {
+                return zone[1];
+            }
         }
-    }
-    if (line == 7) {
-        if (!zone[8].classList.contains('clicked')) {
-            return zone[8];
-        } else if (!zone[2].classList.contains('clicked')) {
-            return zone[2];
-        } else if (!zone[5].classList.contains('clicked')) {
-            return zone[5];
+        if (line == 7) {
+            if (!zone[8].classList.contains('clicked')) {
+                return zone[8];
+            } else if (!zone[2].classList.contains('clicked')) {
+                return zone[2];
+            } else if (!zone[5].classList.contains('clicked')) {
+                return zone[5];
+            }
         }
-    }
+    }    
 }
 
 reset();
-msg.style.display = 'none';
 end.style.display = 'none';
 
 function initialize() {
     for (let i = 0; i < zone.length; i++) {
         zone[i].classList.remove('clicked');
-        zone[i].classList.remove('player-1');
-        zone[i].classList.remove('player-2');
+        zone[i].classList.remove(firstPlayer);
+        zone[i].classList.remove(secondPlayer);
         zone[i].classList.remove('winner-1');
         zone[i].classList.remove('winner-2');
     }
     player.style.display = 'block';
     player.innerHTML = players[0];
-    player.style.color = 'cornflowerblue';
-    msg.style.display = 'block';
-    msg.innerHTML = 'اللعبة مستمرة';
-
+    player.style.color = color_2;
 }
 
 function play() {
     for (let i = 0; i < zone.length; i++) {
-        zone[i].addEventListener('click', function() {
-            if (!this.classList.contains('clicked')) {
+        zone[i].addEventListener('click', function() {            
+            if (!this.classList.contains('clicked') || (firstPlayer === 'player-2' && moves.length === 0)) {
                 this.classList.add('clicked');
                 moves.push(parseInt(this.dataset.order));
                 step = tictactoe(moves, this);
                 if (step[1] === 'اللعبة مستمرة') {
                     if (step[0] === 'player-1') {
                         player.innerHTML = players[1];
-                        player.style.color = 'coral';
-
+                        player.style.color = color_1;
                         // document.body.addEventListener("click", function(evt) { evt.preventDefault()});
-                        setTimeout(function() {zoneToClick(lineToClick()).click()}, 500);                        
-
+                        setTimeout(function() {zoneToClick(lineToClick(), mode).click()}, 500);         
                     } else {
                         player.innerHTML = players[0];
-                        player.style.color = 'cornflowerblue';
+                        player.style.color = color_2; 
                     }
                 } else {
                     reset();
                     if (step[1] === 'يربح') {
-                        if (step[0] === 'player-1') {
-                            msg.innerHTML = '! ' + players[0] + ' يربح';
+                        if (step[0] === 'player-1') {                            
+                            player.innerHTML = '! ' + players[0] + ' يربح';
+                            player.style.color = color_1;
                             points[0]++;
                             score1.innerHTML = points[0];
                         } else {
-                            msg.innerHTML = '! ' + players[1]  + ' يربح';
+                            player.innerHTML = '! ' + players[1]  + ' يربح';
                             points[1]++;
                             score2.innerHTML = points[1];
                         }
                     } else {
-                        msg.innerHTML = step[1];
+                        player.innerHTML = step[1];
                     }
                     end.style.display= 'flex';                    
                 }
             }
         })
     }
-    warning.innerText = ''
-    inputs[0].value = '';      
+    //warning.innerText = ''
+    inputs.value = '';      
     //inputs[1].value = '';
-    inputs[0].style.borderColor = 'gray';
+    inputs.style.borderColor = 'gray';
     //inputs[1].style.borderColor = 'gray';
-    inputs[0].focus();
+    inputs.focus();
+}
+
+function levelSelected() {
+    for (let i = 0; i < 3; i++) {
+        if (levels[i].checked == true) {
+            return levels[i];
+        }
+    }
 }
 
 function playStart() {
-    if (inputs[0].value.trim() !== inputs[0].value) {
-        inputs[0].value = inputs[0].value.trim();
+    if (inputs.value.trim() !== inputs.value) {
+        inputs.value = inputs.value.trim();
     }
     // if (inputs[1].value.trim() !== inputs[1].value) {
     //     inputs[1].value = inputs[1].value.trim();
     // }
-    if (inputs[0].value === '') {
-        warning.innerHTML = '<span>! أدخل اسمك </span>';
-        inputs[0].value = '';
-        inputs[0].focus();
-        inputs[0].style.borderColor = 'orangered';
+    if (inputs.value === '') {
+        //warning.innerHTML = '<span>! أدخل اسمك </span>';
+        inputs.value = '';
+        inputs.style.borderColor = 'orangered';
         //inputs[1].style.borderColor = 'gray';
     // } else if (inputs[1].value === '') {
     //     warning.innerHTML = '! أدخل اسم <span>اللاعب الثاني</span>';
@@ -272,32 +322,41 @@ function playStart() {
     //     inputs[1].focus();
     //     inputs[0].style.borderColor = 'orangered';
     //     inputs[1].style.borderColor = 'orangered';
-    //
+    //    
     } else {
-        players.push(inputs[0].value);
+        players.push(inputs.value);
         players.push('الروبوت');
-        name1.innerHTML = inputs[0].value;
+        name1.innerHTML = inputs.value;
         name2.innerHTML = 'الروبوت';
+        mode = levelSelected().id;
         initialize();
         begin.style.display = 'none';
+        blockLevels.style.display = 'none';
         play();
     }
 }
 
 function playReplay() {
     initialize();
+    blockLevels.style.display = 'none';
     begin.style.display = 'none';
     end.style.display = 'none';
     board = [['', '', ''], ['', '', ''], ['', '', '']];
-    moves = [];  
+    moves = [];
+    [firstPlayer, secondPlayer] = [secondPlayer, firstPlayer];
+    //[players[0], players[1]] = [players[1], players[0]];    
     play();
+    if (firstPlayer === 'player-2' && moves.length === 0) {
+        setTimeout(function() {zoneToClick(lineToClick(), mode).click()}, 500); 
+    }
 }
 
 function playRestart() {
     initialize();
     begin.style.display = 'flex';
+    blockLevels.style.display = 'block';
+    levels[1].checked == true;
     reset();
-    msg.style.display = 'none';
     end.style.display = 'none';
     board = [['', '', ''], ['', '', ''], ['', '', '']];
     moves = [];
@@ -349,7 +408,7 @@ var tictactoe = function(moves, currentElement) {
     }
 
 var getplayer = function(moveIndex) {
-    return moveIndex % 2 === 0 ? 'player-1' : 'player-2';
+    return moveIndex % 2 === 0 ? firstPlayer : secondPlayer;
 }
 
 var playMove = function (player, move) {    
